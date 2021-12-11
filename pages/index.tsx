9bd7c170358f0857af38ba23d2e96f3ab9c1accd
AyamotoKohei <koh14taka2@gmail.com>
2021-12-11 21:09:28 +0900

    add: ダイナミックルーティングの学習

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
 
+const products = [{ name: "bag" }, { name: "shoes" }, { name: "socks" }];
+
 const Home = () => {
   return (
     <div>
       <ul>
+        {products.map((products) => {
+          return (
+            <li key={products.name}>
+              <Link href={`/products/${products.name}`}>
+                <a>{products.name}</a>
+              </Link>
+            </li>
+          );
+        })}
         <li>
-          <Link href="/about">
+          <Link href={"/about"}>
             <a>About</a>
           </Link>
         </li>
