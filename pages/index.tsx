d67ed46bab38390c0b321c069eee08a7a22735f7
AyamotoKohei <koh14taka2@gmail.com>
2021-12-11 21:04:03 +0900

    add: Linkコンポーネントの学習

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,18 @@
-const Home = () => <h1>Hello Next.js</h1>;
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+      <h1>Hello Next.js</h1>
+    </div>
+  );
+};
 
 export default Home;
